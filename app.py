79d2a91b233d93858a972b87854bdf2ea0bc9804
Shivangi Gupta <shivangi.gupta9820@gmail.com>
2020-10-22 00:53:58 +0530

    password encrypted, redirection corrected

--- app.py
+++ app.py
@@ -50,13 +50,16 @@ def login():
     user = Teacher.query.filter_by(username=uname).first()
 
     # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return render_template('login.html') # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    return render_template('postlogin.html')
+    return redirect('/dashboard')
+
+@app.route("/dashboard")
+def dahboard():
+    return render_template("dashboard.html")
 
 # Create a Teacher
 @app.route('/teacher', methods=['POST'])
@@ -69,14 +72,14 @@ def add_teacher():
     user = Teacher.query.filter_by(username=usernamef).first()  # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists, Try with a different username or login!')
+        flash('Username already exists, Try with a different username or login!')
         return render_template('index.html')
     
     new_teacher = Teacher(usernamef, namef,generate_password_hash(passwordf, method='sha256'))
     db.session.add(new_teacher)
     db.session.commit()
 
-    return render_template('login.html')
+    return redirect('/login')
 
 # Get All Teachers
 @app.route('/teacher', methods=['GET'])
@@ -89,6 +92,15 @@ def get_teachers():
 @app.route('/teacher/<username>', methods=['GET'])
 def get_teacher(username):
     teacher = Teacher.query.get(username)
+    return t_schema.jsonify(teacher)
+
+
+# Delete Product
+@app.route('/teacher/<username>', methods=['DELETE'])
+def delete_teacher(username):
+    teacher = Teacher.query.get(username)
+    db.session.delete(teacher)
+    db.session.commit()
     return t_schema.jsonify(teacher)
 
 if __name__ == '__main__':
