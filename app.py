cac7e85ccc92a60d18d4d5b590830cf2da99d0d4
Shivangi Gupta <shivangi.gupta9820@gmail.com>
2020-10-22 17:28:25 +0530

    added routes for dashboard CURD operations teacher

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template,redirect, url_for,flash
-from flask_login import UserMixin
+from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow
@@ -37,6 +37,32 @@ class TeacherSchema(ma.Schema):
 t_schema = TeacherSchema()
 ts_schema = TeacherSchema(many=True)
 
+#Course Class/Model
+class Course(db.Model):
+    courseid = db.Column(db.String(100), primary_key=True)
+    coursename = db.Column(db.String(100))
+    semester = db.Column(db.String(128))
+    credit = db.Column(db.String(128))
+    hasquiz = db.Column(db.String(128))
+
+    def __init__(self,courseid,coursename,semester,credit,hasquiz):
+        self.courseid=courseid
+        self.coursename=coursename
+        self.semester=semester
+        self.credit = credit
+        self.hasquiz= hasquiz
+
+# Course Schema
+class CourseSchema(ma.Schema):
+  class Meta:
+    fields = ('courseid', 'coursename', 'semester','credit','hasquiz')
+
+# init Schema
+course_schema = CourseSchema()
+courses_schema = CourseSchema(many=True)
+
+
+#Authtication routes
 
 @app.route('/')
 def index():
@@ -55,11 +81,7 @@ def login():
         return render_template('login.html') # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    return redirect('/dashboard')
-
-@app.route("/dashboard")
-def dahboard():
-    return render_template("dashboard.html")
+    return redirect('/teacherdashboard')
 
 # Create a Teacher
 @app.route('/teacher', methods=['POST'])
@@ -95,13 +117,45 @@ def get_teacher(username):
     return t_schema.jsonify(teacher)
 
 
-# Delete Product
+# Delete Teacher
 @app.route('/teacher/<username>', methods=['DELETE'])
 def delete_teacher(username):
     teacher = Teacher.query.get(username)
     db.session.delete(teacher)
     db.session.commit()
     return t_schema.jsonify(teacher)
+
+
+
+#Dashboard Routes For Teacher
+
+@app.route("/teacherdashboard")
+def dashboard():
+    return render_template("t_dashboard.html")
+
+@app.route("/addcourse")
+def addc():
+    return render_template("addcourse.html")
+
+@app.route("/editcourse")
+def editc():
+    return render_template("editcourse.html")
+
+@app.route("/delcourse")
+def delc():
+    return render_template("delcourse.html")
+
+@app.route("/viewcourse")
+def viewc():
+    return render_template("viewcourse.html")
+
+#Quiz Portal Routes for Teacher
+
+@app.route("/quizdashboard")
+def quiz_dashboard():
+    return render_template("quizdashboard.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug= True)
